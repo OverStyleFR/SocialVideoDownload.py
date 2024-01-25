917952ba3817d64483951a23975724a7ee02aade
tomv <personnal@tomv.ovh>
2024-01-25 15:31:49 +0100

    Update | Download with a unique ID ✓ #18

--- main.py
+++ main.py
@@ -240,49 +240,67 @@ def music(update, context):
 
     max_retries = 3  # Nombre maximum de réessais
     current_retry = 0
+    music_path = None  # Initialiser le chemin du fichier musique
 
     reply_message = context.bot.send_message(chat_id=update.message.chat_id, text="Downloading. processing...", reply_to_message_id=update.message.message_id)
     # Log pour enregistrer que le téléchargement est en cours
     console_logger.info(f"Downloading with /music and the link :  {link} || from {update.message.from_user.username}")
 
-    while current_retry < max_retries:
-        try:
-            ffmpeg_location = "ffmpeg-6.1-amd64-static/ffmpeg"
-            result = subprocess.run(["./yt-dlp", "--extract-audio", "--audio-format", "mp3", "--ffmpeg-location", ffmpeg_location, "-o", "downloaded_music.%(ext)s", link], capture_output=True, text=True)
-            output = result.stdout.strip() if result.stdout else result.stderr.strip()
+    try:
+        link_hash = hashlib.md5(link.encode()).hexdigest()  # Générer un identifiant unique basé sur le hachage du lien
+        music_path = os.path.join(download_temp_folder, f"{link_hash}.mp3")
 
-            context.bot.send_message(chat_id=update.message.chat_id, text=output)
-            context.bot.send_message(chat_id=update.message.chat_id, text="Download finish. Processing in progress...", reply_to_message_id=reply_message.message_id)
-            console_logger.info(f"Download finish with the link : {link} || from {update.message.from_user.username}")
+        if os.path.exists(music_path):
+            # Si le fichier existe déjà, envoyer directement la musique
+            music = open(music_path, "rb")
+            context.bot.send_audio(chat_id=update.message.chat_id, audio=InputFile(music), caption="Here's your music (already downloaded)", reply_to_message_id=reply_message.message_id)
 
-            music_path = "downloaded_music.mp3"
-            if os.path.exists(music_path):
-                music = open(music_path, "rb")
-                context.bot.send_audio(chat_id=update.message.chat_id, audio=InputFile(music), caption="Here's your music", reply_to_message_id=reply_message.message_id)
+            # Log lorsque la musique est envoyée sans téléchargement
+            console_logger.info(f"Music sent directly from cache to {update.message.from_user.username}")
+            
+            music.close()
+        else:
+            # Sinon, procéder au téléchargement
+            while current_retry < max_retries:
+                try:
+                    ffmpeg_location = "ffmpeg-6.1-amd64-static/ffmpeg"
+                    result = subprocess.run(["./yt-dlp", "--extract-audio", "--audio-format", "mp3", "--ffmpeg-location", ffmpeg_location, "-o", music_path, link], capture_output=True, text=True)
+                    output = result.stdout.strip() if result.stdout else result.stderr.strip()
+                    context.bot.send_message(chat_id=update.message.chat_id, text=output, reply_to_message_id=reply_message.message_id)
 
-                # Log lorsque la musique est envoyée (uploadée)
-                console_logger.info(f"Music send successfully to {update.message.from_user.username} ⇒ /music")
+                    if os.path.exists(music_path):
+                        music = open(music_path, "rb")
+                        context.bot.send_audio(chat_id=update.message.chat_id, audio=InputFile(music), caption="Here's your music", reply_to_message_id=reply_message.message_id)
 
-                music.close()
-                os.remove(music_path)
-                break  # Sortir de la boucle en cas de succès
-            else:
-                context.bot.send_message(chat_id=update.message.chat_id, text="Erreur: La musique téléchargée n'a pas été trouvée.", reply_to_message_id=reply_message.message_id)
-                # Log en cas d'échec de l'upload de la musique
-                console_logger.error(f"Échec de l'upload de la musique à {update.message.from_user.username}")
-                current_retry += 1
-                context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
-        except urllib3.exceptions.HTTPError as http_error:
-            context.bot.send_message(chat_id=update.message.chat_id, text=f"Erreur HTTP lors du téléchargement de la musique. Tentative {current_retry + 1}/{max_retries}.", reply_to_message_id=reply_message.message_id)
-            console_logger.error(f"Erreur HTTP lors du téléchargement de la musique depuis le lien {link}: {str(http_error)}")
-            current_retry += 1
-            context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
-        except Exception as e:
-            context.bot.send_message(chat_id=update.message.chat_id, text=f"Erreur lors de l'exécution de la commande: {str(e)}", reply_to_message_id=reply_message.message_id)
-            # Log en cas d'erreur lors du téléchargement de la musique
-            console_logger.error(f"Erreur lors du téléchargement de la musique depuis le lien {link}: {str(e)}")
-            current_retry += 1
-            context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
+                        # Log lorsque la musique est envoyée (uploadée)
+                        console_logger.info(f"Music send successfully to {update.message.from_user.username} ⇒ /music")
+
+                        music.close()
+                        break  # Sortir de la boucle en cas de succès
+                    else:
+                        context.bot.send_message(chat_id=update.message.chat_id, text="Erreur: La musique téléchargée n'a pas été trouvée.", reply_to_message_id=reply_message.message_id)
+                        # Log en cas d'échec de l'upload de la musique
+                        console_logger.error(f"Échec de l'upload de la musique à {update.message.from_user.username}")
+                        current_retry += 1
+                        context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
+                except urllib3.exceptions.HTTPError as http_error:
+                    context.bot.send_message(chat_id=update.message.chat_id, text=f"Erreur HTTP lors du téléchargement de la musique. Tentative {current_retry + 1}/{max_retries}.", reply_to_message_id=reply_message.message_id)
+                    console_logger.error(f"Erreur HTTP lors du téléchargement de la musique depuis le lien {link}: {str(http_error)}")
+                    current_retry += 1
+                    context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
+                except Exception as e:
+                    context.bot.send_message(chat_id=update.message.chat_id, text=f"Erreur lors de l'exécution de la commande: {str(e)}", reply_to_message_id=reply_message.message_id)
+                    # Log en cas d'erreur lors du téléchargement de la musique
+                    console_logger.error(f"Erreur lors du téléchargement de la musique depuis le lien {link}: {str(e)}")
+                    current_retry += 1
+                    context.bot.send_message(chat_id=update.message.chat_id, text=f"Réessai {current_retry}/{max_retries}...", reply_to_message_id=reply_message.message_id)
+    except Exception as e:
+        # Log en cas d'erreur pendant l'exécution de la commande
+        console_logger.error(f"Erreur pendant l'exécution de la commande /music : {str(e)}")
+    finally:
+        # Supprimer le fichier temporaire uniquement en cas d'échec
+        if music_path and not os.path.exists(music_path):
+            os.remove(music_path)
 
 
 def read_token():
