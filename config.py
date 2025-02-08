d7ae52dd065331ae927b5d1e2dc4778cff611909
overstylefr <personnal@tomv.ovh>
2025-02-08 02:35:49 +0100

    V0.8-7

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-VERSION = "V.8-2"
+VERSION = "V.8-7"
 DEVELOPED_BY = "Tom V. | OverStyleFR"
 FFMPEG_PATH = "ffmpeg/ffmpeg-7.0.2-amd64-static/ffmpeg"  # Change cette valeur si nécessaire (chemin complet vers l'exécutable ffmpeg)
