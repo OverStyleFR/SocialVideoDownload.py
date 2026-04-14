fb1f5be70d9f5a4cca563250a802326426a8aa59
Tom V. | OvᴇʀStylᴇFR <94577363+OverStyleFR@users.noreply.github.com>
2026-04-14 09:47:21 +0200

    feat: migrate config to .env file with auto-generation

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-# config.py
-VERSION = "V.8-7"
-DEVELOPED_BY = "Tom V. | OverStyleFR"
-FFMPEG_PATH = "ffmpeg/ffmpeg-7.0.2-amd64-static/ffmpeg"  # Change cette valeur si nécessaire (chemin complet vers l'exécutable ffmpeg)
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+VERSION = os.getenv("VERSION", "V.8-7")
+DEVELOPED_BY = os.getenv("DEVELOPED_BY", "Tom V. | OverStyleFR")
+FFMPEG_PATH = os.getenv("FFMPEG_PATH", "ffmpeg/ffmpeg-7.0.2-amd64-static/ffmpeg")  # Change cette valeur si nécessaire (chemin complet vers l'exécutable ffmpeg)
