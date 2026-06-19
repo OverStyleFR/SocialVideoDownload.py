21ad3dd71055d389380bf25794cf449fbd33f194
Tom V. | OvᴇʀStylᴇFR <personnal@tomv.ovh>
2026-06-19 10:24:11 +0200

    feat: enhance ProgressFile with optional progress callback
    - Add callback parameter called on each progress interval
    - Reduce default progress_interval from 20% to 10%

--- utils/progress_file.py
+++ utils/progress_file.py
@@ -3,13 +3,14 @@ import math
 from utils.logger import console_logger
 
 class ProgressFile:
-    def __init__(self, filename, progress_interval=20):
+    def __init__(self, filename, progress_interval=10, callback=None):
         self.filename = filename
         self.f = open(filename, "rb")
         self.total = os.path.getsize(filename)
         self.read_bytes = 0
         self.last_percent = 0
-        self.progress_interval = progress_interval  # Intervalle de 20% par défaut
+        self.progress_interval = progress_interval
+        self.callback = callback
 
     def read(self, size=-1):
         data = self.f.read(size)
@@ -19,11 +20,12 @@ class ProgressFile:
             if percent - self.last_percent >= self.progress_interval:
                 self.last_percent = percent
                 console_logger.info(f"[UPLOAD] Envoi du fichier {self.filename} : {percent}% complété.")
+                if self.callback:
+                    self.callback(percent)
         return data
 
     def close(self):
         self.f.close()
 
     def __getattr__(self, attr):
-        # Permet d'accéder aux autres attributs du fichier
         return getattr(self.f, attr)
